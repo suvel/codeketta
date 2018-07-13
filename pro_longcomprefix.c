df6411a5d29970521e6cfbf9f360bb3483046578
suvel <40818146+suvel@users.noreply.github.com>
2018-07-13 07:02:02 +0000

    not yet completed

---
+++ pro_longcomprefix.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<string.h>
+void main(){
+int N,i;
+printf("number");
+scanf("%d",&N);
+char name[N][100000];
+for(i=0;i<N;i++){
+scanf("%s",name[i]);
+}
+int len,M=0;
+for(i=0;i<N;i++){
+len=strlen(name[i]);
+if(M==0){
+M=len;
+}
+if(M<len){
+M=len;
+}
+}
+
+char ch,newstr[len];
+for(i=0;i<len;i++){
+int tempN=0;
+int count=0;
+char MK=NULL;
+ while(tempN<N){
+if(MK==0 || MK==name[tempN][i]){
+MK=name[tempN][i];
+count++;
+printf("%d %c\n",count,MK);
+}
+tempN++;
+ }
+if(count==N){
+printf("%C",MK);
+}
+}
+
+}
