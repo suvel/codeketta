5602c9c9845896e5db7bc8e95430cf8579ed19a4
suvel <40818146+suvel@users.noreply.github.com>
2018-07-17 09:11:37 +0000

    Create largersubstring.py

---
+++ largersubstring.py
@@ -0,0 +1,17 @@
+i=str(input())
+inew=""
+large_str=""
+for j in range (0,len(i)):
+
+	if(inew==""):
+		inew=inew+i[j]
+	elif i[j] in inew:
+		if(len(inew)>len(large_str)):
+			large_str=inew
+		inew="";
+		
+	elif j==len(i)-1:
+		large_str=inew
+	else:
+		inew=inew+i[j]
+print(large_str)
