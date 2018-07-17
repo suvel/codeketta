4f64d9f9acb316b9de55896ba83127c73e2a17a3
suvel <40818146+suvel@users.noreply.github.com>
2018-07-17 23:09:03 +0530

    need to be updated

---
+++ str2xy.py
@@ -0,0 +1,22 @@
+a=str(input())
+b=str(input())
+la=len(a)
+lb=len(b)
+delete=1
+update=1
+insert=1
+if la>lb:
+	for char in range(la-1,la-lb-1,-1):
+		
+		a = a.replace(a[char],"")
+		delete=delete+1
+for i in range(0,lb):
+	if i>=la:
+		a=a+b[i]
+		insert=insert+1
+	if b[i]!=a[i]:
+		print("a--"+a[i]),
+		print("b--"+b[i])
+		update=update+1
+print(delete+update+insert),
+print(a)
