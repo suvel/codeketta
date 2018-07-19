9641a2005e10152676d7c87c9cdc58ca8b8cd39f
suvel <40818146+suvel@users.noreply.github.com>
2018-07-19 21:16:20 +0530

    Update tornmentN.py

--- tornmentN.py
+++ tornmentN.py
@@ -1,6 +1,19 @@
-val=int(input())
-if (3<=val<=100000 and val%2==0):
+
+def check():
+	faz2=2
+	count=0
+	if(3<=val<=100000 and not(val & (val-1))):
 		print("0")
-else:
-	print("1")
+	else:
+		while(faz2<val):
+			faz2=faz2*2
+			count+=1
+		print(count-1)
+		
 	
+
+val=int(input())
+if(3<=val<=100000):
+	check()
+else:
+	print("enter value between 3-100000")
