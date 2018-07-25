f57b06b2569f8a5b20767a8a97ced1dfdb75fafc
suvel <40818146+suvel@users.noreply.github.com>
2018-07-25 08:18:02 +0000

    Update hunter_8.py

--- hunter_8.py
+++ hunter_8.py
@@ -5,5 +5,5 @@ for i in range(N):
 	for j in range(i+1,N):
 		for k in range(j+1,N):
 			if(int(A[i])+int(A[j])==int(A[k])):
-				print(str(A[i])+""+str(A[j])+""+str(A[k]))
+				print(str(A[i])+""+str(A[j])+""+str(A[k]),end="\n")
 				
