746408751cbf7f7f07573d85fcbc6962e5fb5e65
suvel <40818146+suvel@users.noreply.github.com>
2018-07-26 04:54:11 +0000

    Update pro_62.py

--- pro_62.py
+++ pro_62.py
@@ -3,16 +3,24 @@ def chek(s):
         return 1
     else:
         return 0
-   
-A=str(input())
-s=list()
-for i in range (len(A)):
-    s.append(A[i])
-for i in range (len(A)):   
-    sr= ''.join(s)
-    if(chek(sr)==1):
-        pass
-    else:   
-        s.pop()
+def ck(A):
+	s=list()
+	for i in range (len(A)):
+		s.append(A[i])
+	for i in range (len(A)):
+		sr= ''.join(s)
+		if(chek(sr)==1):
+			pass
+		else:
+			s.pop()
 
-print(len(A)-len(s))    
+	return (len(A)-len(s))  
+
+	
+A=str(input())
+if (ck(A)<ck(A[::-1])):
+	print(ck(A))
+else:
+	print(ck(A[::-1]))
+	
+	
