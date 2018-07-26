603cba4d99de511a05708fcd25c16059ad7a3b87
suvel <40818146+suvel@users.noreply.github.com>
2018-07-26 08:00:25 +0000

    Create pro_51.py

---
+++ pro_51.py
@@ -0,0 +1,26 @@
+def check(i):
+	if i>0:
+		return 1
+	if i<0:
+		return -1
+N=int(input())
+A=[]
+A=input().split()
+count=0
+charge=0
+for i in range(N):
+	count=0
+	charge=0
+	if(charge==0):
+		charge=check(int(A[i]))
+		#print(charge)
+		count=count+1
+	for h in range(i+1,N):
+		if(check(int(A[h]))!=charge):
+			count=count+1
+			charge=check(int(A[h]))
+		else:
+			break
+	print(count,end=' ')	
+		
+			
