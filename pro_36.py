f5e5362fd4f833e73d5a93f2977cd6c22341427e
suvel <40818146+suvel@users.noreply.github.com>
2018-07-27 06:02:45 +0000

    Update pro_36.py

--- pro_36.py
+++ pro_36.py
@@ -2,9 +2,9 @@ N=int(input())
 A=[]
 A=input().split()
 count=0
-for i in range(N):
-	for j in range(i+1,N):
-		for k in range(j+1,N):
+for i in range(len(A)):
+	for j in range(i+1,len(A)):
+		for k in range(j+1,len(A)):
 			if(int(A[i])>int(A[j])>int(A[k])):
 				count=count+1
 print(count)
