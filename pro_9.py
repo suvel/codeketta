d41bef978b4328bf709c11e3c51dbbb172c6199a
suvel <40818146+suvel@users.noreply.github.com>
2018-07-28 08:35:54 +0530

    Update pro_9.py

--- pro_9.py
+++ pro_9.py
@@ -1,17 +1,2 @@
-#include <iostream>
-using namespace std;
-int fun(int i){
-	if(3<=i and i<=7);
-	return i;
-	if(i>7){
-	i=fun(i-4)+4;	
-	}
-	return i;
-}
-int main() {
-	int i;
-	cin>>i;
-	i=fun(i);
-	cout<<i;
-	return 0;
-};
+N=int(input())
+print(N)
